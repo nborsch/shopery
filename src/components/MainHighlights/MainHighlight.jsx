16680d76aff4098e08fc923a5c201e767b52a24c
Nadia Borsch <work@nborsch.com>
2025-10-01 16:29:40 -0500

    Initial Commit

---
+++ src/components/MainHighlights/MainHighlight.jsx
@@ -0,0 +1,6 @@
+import React from "react"
+
+export default function MainHighlight(){
+
+    return 
+}
---
+++ src/components/Testimonials/Testimonials.jsx
@@ -0,0 +1,6 @@
+import React from "react"
+
+export default function Testimonials(){
+
+    return 
+}
