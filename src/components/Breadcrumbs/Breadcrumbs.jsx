84a188dad21067d7c49f52ad1d48e3742f2df5e6
Nadia Borsch <work@nborsch.com>
2025-10-09 15:44:51 -0500

    BUILD Breadcrumbs component UI

--- src/components/Breadcrumbs/Breadcrumbs.jsx
+++ src/components/Breadcrumbs/Breadcrumbs.jsx
@@ -1,13 +1,22 @@
 import React from "react"
 import classes from "./Breadcrumbs.module.css"
+import { FaHouse } from "react-icons/fa6"
 
 export default function Breadcrumbs(){
 
   return <>
     <div className={classes.breadcrumbsContainer}>
-      <div className={classes.breadcrumbs}>
-        Breadcrumbs
-      </div>
-    </div>
+      <div className={classes.breadcrumbsContainerGradient}>
+        <nav className={classes.breadcrumbs}>
+          <a href="ths" className={classes.breadcrumbsLink}>
+            <FaHouse size="24px"/>
+          </a> 
+          {'>'} 
+          <a href="ths" className={classes.breadcrumbsLink}>Categories</a> 
+          {'>'} 
+          <a href="trsh" className={classes.breadcrumbsLink}>Vegetables</a>
+        </nav>
+      </div> {/* breadcrumbsContainerGradient */}
+    </div> {/* breadcrumbsContainer */}
   </>
 }
