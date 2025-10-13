0c3a76714b70c531edbc06e0113608ba56a47fa0
Nadia Borsch <work@nborsch.com>
2025-10-13 14:44:25 -0500

    CLEANUP Footer and Newsletter components

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -10,7 +10,6 @@ import {
 
 } from "react-icons/fa6"
 import classes from "./Footer.module.css"
-import Logo from "../Logo"
 import NewsLetter from "../NewsLetter/Newsletter"
 import Instagram from "../Instagram/Instagram"
 import PhoneNumber from "../PhoneNumber"
@@ -20,30 +19,19 @@ export default function Footer(){
 
   return (
   <footer>
-    <div className={classes.newsContainer}>
-      <div className={classes.news}>
-        <Logo width={225} />
-        <div>
-          <p className={classes.newsPara}>Subscribe to our newletter</p>
-          <span className={classes.newsSpan}>Pellentesque eu nibh eget mauris congue mattis matti.</span>
-        </div>
-        <div>
-            <NewsLetter />
-        </div>
-        </div>
-    </div>
-    <div className={classes.infoContainer}>
+    <NewsLetter />
+    <div className={classes.container}>
       <div className={classes.info}>
-        <div className={classes.infoTop}>
-          <div className={classes.infoTopAbout}>
+        <div className={classes.top}>
+          <div className={classes.topAbout}>
             <h3>About Shopery</h3>
             <p>Morbi cursus porttitor enim lobortis molestie. Duis gravida turpis dui, eget bibendum magna congue nec.</p>
             <PhoneNumber /> or <Email />
           </div>
-          <nav className={classes.infoTopLinks}>
+          <nav className={classes.topLinks}>
             <div>
               <h3>My account</h3>
-              <ul className={classes.infoTopLinksList}>
+              <ul className={classes.topLinksList}>
                 <li><a href="">Account</a></li>
                 <li><a href="">Order history</a></li>
                 <li><a href="">Shopping Cart</a></li>
@@ -52,7 +40,7 @@ export default function Footer(){
             </div>
             <div>
               <h3>Help</h3>
-              <ul className={classes.infoTopLinksList}>
+              <ul className={classes.topLinksList}>
                 <li><a href="">Contact us</a></li>
                 <li><a href="">Terms & conditions</a></li>
                 <li><a href="">Privacy policy</a></li>
@@ -60,7 +48,7 @@ export default function Footer(){
             </div>
             <div>
               <h3>Pages</h3>
-              <ul className={classes.infoTopLinksList}>
+              <ul className={classes.topLinksList}>
                 <li><a href="">About</a></li>
                 <li><a href="">Shop</a></li>
                 <li><a href="">Track order</a></li>
@@ -69,15 +57,15 @@ export default function Footer(){
           </nav>
           <Instagram />
         </div> {/* infoTop */}
-        <div className={classes.infoBottom}>
-          <div className={classes.infoBottomSocials}>
+        <div className={classes.bottom}>
+          <div className={classes.bottomSocials}>
             <a href="https://facebook.com"><FaFacebook size="30px" /></a>
             <a href="https://x.com"><FaSquareXTwitter size="30px" /></a>
             <a href="https://pinterest.com"><FaPinterestP size="30px" /></a>
             <a href="https://instagramcom"><FaInstagram size="30px" /></a>
           </div>
           <p>Shopery eCommerce by Nadia Borsch © {new Date().getFullYear()}. All Rights Reserved</p>
-          <div className={classes.infoBottomFlags}>
+          <div className={classes.bottomFlags}>
             <a href="https://www.apple.com/apple-pay/"><FaApplePay size="45px" /></a>
             <a href="https://usa.visa.com/"><FaCcVisa size="45px" /></a>
             <a href="https://www.discover.com/"><FaCcDiscover size="45px" /></a>
--- src/components/NewsLetter/Newsletter.jsx
+++ src/components/NewsLetter/Newsletter.jsx
@@ -1,12 +1,24 @@
 import React from "react"
+import Logo from "../Logo"
 import classes from "./Newsletter.module.css"
 
 export default function Newsletter(){
 
-  return <>
-    <form>
-      <input className={classes.footerNewsInput} placeholder="Search" />
-      <button className={classes.footerNewsButton}>Subscribe</button>
-    </form>
-  </>
+  return (
+    <div className={classes.container}>
+      <div className={classes.news}>
+        <Logo width={225} />
+        <div>
+          <p className={classes.para}>Subscribe to our newletter</p>
+          <span className={classes.span}>Pellentesque eu nibh eget mauris congue mattis matti.</span>
+        </div>
+        <div>
+          <form>
+            <input className={classes.input} placeholder="Search" />
+            <button className={classes.button}>Subscribe</button>
+          </form>
+        </div>
+      </div>
+    </div>
+  )
 }
