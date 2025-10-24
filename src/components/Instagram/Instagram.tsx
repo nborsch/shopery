06eae5fcce77acc3d783ba4f06a1a6b4a69062c4
Nadia Borsch <work@nborsch.com>
2025-10-24 17:13:40 -0500

    BUILD Instagram component error handling

--- src/components/Instagram/Instagram.tsx
+++ src/components/Instagram/Instagram.tsx
@@ -1,33 +1,36 @@
-import React from "react";
-import classes from "./Instagram.module.css";
+import React from "react"
+import classes from "./Instagram.module.css"
 
 type Photo = {
-  id: number;
-  width: number;
-  height: number;
-  url: string;
-  photographer: string;
-  photographer_url: string;
-  photographer_id: number;
-  avg_color: string;
+  id: number
+  width: number
+  height: number
+  url: string
+  photographer: string
+  photographer_url: string
+  photographer_id: number
+  avg_color: string
   src: {
-    original: string;
-    large2x: string;
-    large: string;
-    medium: string;
-    small: string;
-    portrait: string;
-    landscape: string;
-    tiny: string;
-  };
-  liked: boolean;
-  alt: string;
-};
+    original: string
+    large2x: string
+    large: string
+    medium: string
+    small: string
+    portrait: string
+    landscape: string
+    tiny: string
+  }
+  liked: boolean
+  alt: string
+}
 
 export default function Instagram() {
-  const [photos, setPhotos] = React.useState<Photo[] | null>(null);
+  const [photos, setPhotos] = React.useState<Photo[] | null>(null)
 
   React.useEffect(() => {
+    // preparing to close connection on unmount
+    const controller = new AbortController()
+
     async function fetchPhotos() {
       const photosResponse = await fetch(
         "https://api.pexels.com/v1/search?query=healthy&orientation=square&per_page=8",
@@ -36,20 +39,35 @@ export default function Instagram() {
           headers: {
             Authorization: import.meta.env.VITE_PEXELS_API_KEY,
           },
+          signal: controller.signal,
         }
-      );
-      const photosData = await photosResponse.json();
-      setPhotos(photosData.photos);
+      )
+
+      if (!photosResponse.ok) {
+        console.log(photosResponse)
+        return
+      }
+
+      const photosData = await photosResponse.json()
+      setPhotos(photosData.photos)
     }
 
-    fetchPhotos();
-  }, []);
+    fetchPhotos()
+
+    return () => controller.abort()
+  }, [])
 
   const photosEls = photos
-    ? photos.map((photoData: Photo): React.JSX.Element => {
-        return <img src={`${photoData.src.small}`} className={classes.img} />;
+    ? photos.map((photoData: Photo, index: number): React.JSX.Element => {
+        return (
+          <img
+            src={`${photoData.src.small}`}
+            className={classes.img}
+            key={index}
+          />
+        )
       })
-    : null;
+    : null
 
   return (
     <>
@@ -58,5 +76,5 @@ export default function Instagram() {
         {photosEls ? photosEls : null}
       </div>
     </>
-  );
+  )
 }
