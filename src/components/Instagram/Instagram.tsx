85fc50e54f32cd0f71c3352f42532a404c11c947
Nadia Borsch <work@nborsch.com>
2025-10-24 16:39:29 -0500

    BUILD Instagram component logic

--- src/components/Instagram/Instagram.tsx
+++ src/components/Instagram/Instagram.tsx
@@ -1,19 +1,62 @@
-import React from "react"
-import classes from "./Instagram.module.css"
+import React from "react";
+import classes from "./Instagram.module.css";
 
-export default function Instagram(){
+type Photo = {
+  id: number;
+  width: number;
+  height: number;
+  url: string;
+  photographer: string;
+  photographer_url: string;
+  photographer_id: number;
+  avg_color: string;
+  src: {
+    original: string;
+    large2x: string;
+    large: string;
+    medium: string;
+    small: string;
+    portrait: string;
+    landscape: string;
+    tiny: string;
+  };
+  liked: boolean;
+  alt: string;
+};
 
-  return <>
-    <div className={classes.instagram}>
-      <h3 className={classes.instagramTitle}>Instagram</h3>
-      <img src="/img/temp-insta1.png" />
-      <img src="/img/temp-insta2.png" />
-      <img src="/img/temp-insta3.png" />
-      <img src="/img/temp-insta4.png" />
-      <img src="/img/temp-insta5.png" />
-      <img src="/img/temp-insta6.png" />
-      <img src="/img/temp-insta7.png" />
-      <img src="/img/temp-insta8.png" />
-    </div>
-  </>
+export default function Instagram() {
+  const [photos, setPhotos] = React.useState<Photo[] | null>(null);
+
+  React.useEffect(() => {
+    async function fetchPhotos() {
+      const photosResponse = await fetch(
+        "https://api.pexels.com/v1/search?query=healthy&orientation=square&per_page=8",
+        {
+          method: "GET",
+          headers: {
+            Authorization: import.meta.env.VITE_PEXELS_API_KEY,
+          },
+        }
+      );
+      const photosData = await photosResponse.json();
+      setPhotos(photosData.photos);
+    }
+
+    fetchPhotos();
+  }, []);
+
+  const photosEls = photos
+    ? photos.map((photoData: Photo): React.JSX.Element => {
+        return <img src={`${photoData.src.small}`} className={classes.img} />;
+      })
+    : null;
+
+  return (
+    <>
+      <div className={classes.instagram}>
+        <h3 className={classes.instagramTitle}>Instagram</h3>
+        {photosEls ? photosEls : null}
+      </div>
+    </>
+  );
 }
