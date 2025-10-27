20d09335900b254b1fc5f5e9f05c9f4bf7ba534c
Nadia Borsch <work@nborsch.com>
2025-10-27 17:46:00 -0500

    BUILD Newsletter component logic

--- src/components/NewsLetter/Newsletter.tsx
+++ src/components/NewsLetter/Newsletter.tsx
@@ -2,7 +2,29 @@ import React from "react"
 import Logo from "../Logo"
 import classes from "./Newsletter.module.css"
 
-export default function Newsletter(){
+export default function Newsletter() {
+  function getEmail(formData: FormData) {
+    const email = formData.get("email") as string
+    sendEmail(email)
+  }
+
+  function sendEmail(email: string) {
+    async function send() {
+      try {
+        const confirm = await fetch("http://localhost:3000/newsletter", {
+          method: "POST",
+        })
+        if (!confirm) {
+          return new Error("Could not submit form!")
+        }
+        console.log(email)
+      } catch(err) {
+        console.log(err)
+      }
+    }
+
+    send()
+  }
 
   return (
     <div className={classes.container}>
@@ -10,12 +32,20 @@ export default function Newsletter(){
         <Logo width={225} />
         <div>
           <p className={classes.para}>Subscribe to our newletter</p>
-          <span className={classes.span}>Pellentesque eu nibh eget mauris congue mattis matti.</span>
+          <span className={classes.span}>
+            Pellentesque eu nibh eget mauris congue mattis matti.
+          </span>
         </div>
         <div>
-          <form>
-            <input className={classes.input} placeholder="Your email" />
-            <button className={classes.button}>Subscribe</button>
+          <form action={getEmail}>
+            <input
+              name="email"
+              placeholder="Your email"
+              className={classes.input}
+            />
+            <button type="submit" className={classes.button}>
+              Subscribe
+            </button>
           </form>
         </div>
       </div>
